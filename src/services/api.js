570f2ff3c326f910c04d59dc62ea873f7b44fd0f
Thauã Borges <thaua97@gmail.com>
2019-07-11 20:11:01 -0300

    adiciona a url da api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 import { getToken } from './auth'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    //baseURL: 'http://localhost:3333'
+    baseURL: 'https://fastbox-server.herokuapp.com/'
 })
 
 api.interceptors.request.use( async config => {
